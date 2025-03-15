8fef03ffbb27bfbc7003929b2691e26e5a19a375
Zuxriddinoff <zuxriddinovoff@gmail.com>
2025-03-15 13:18:43 +0500

    finishing

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let a = 10
+for (let i = 0; i < 10; i++){
+    console.log("hello world");
+}
+
+
+
+// Zuxriddinov@Lutfiddin MINGW64 ~/onedrive/desktop/bootcamp result/21-dars uyga vazifa (main)
+// $ git log
+// commit f7c65fc0400729daa52a7631962e601d9bbbf548 (HEAD -> main)
+// Author: Zuxriddinoff <zuxriddinovoff@gmail.com>
+// Date:   Sat Mar 15 12:44:36 2025 +0500
+
+//     feature_1
+
+// commit 1ef0bb1efc792dd4eac2288941f7d3de3f6bb2d7 (origin/main)
+// Author: Zuxriddinoff <zuxriddinovoff@gmail.com>
+// Date:   Sat Mar 15 12:35:16 2025 +0500
+
+//     second commit
+
+// commit b63504c4a5a7dd8d800f059ce2acd1d46f0fc500
+// Author: Zuxriddinoff <zuxriddinovoff@gmail.com>
+// Date:   Sat Mar 15 12:33:15 2025 +0500
+
+//     commit
